8e79c8c33567d4f2ac69b0a506e8c65e27dfe052
Sean Idzenga <sean.idzenga@protonmail.com>
2024-08-30 12:18:54 -0400

    Add catppuccin's base class to the document body

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -11,6 +11,9 @@ root.render(
   </React.StrictMode>
 );
 
+// adds the bg-base class to the body ensuring we always have a dark background.
+document.body.classList.add("bg-base");
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
