6dbf9cd40ab81a262992c41ddb9f4f60c3c2d9f4
Sean Idzenga <3802836+seanidzenga@users.noreply.github.com>
2024-08-30 13:58:55 -0400

    Install tailwind and catppuccin (#1)
    * Install tailwind and init
    
    * configure template path
    
    * add tailwind directives to main css
    
    * Add catppuccin theme
    
    * Add tailwind's generated css to app.js
    
    * Add catppuccin's base class to the document body
    
    * Add catppuccin plugin to tailwind config
    
    * import tailwind generated css in index.js instead of app.js
    
    * tailwind imports moved to index.css
    
    * Add appropriate classes to body tag

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './output.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,6 +11,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+document.body.classList.add("macchiato"); // sets the default catppuccin flavour but can be swapped dynamically
+document.body.classList.add("bg-base"); // adds the bg-base class to the body ensuring we always have a dark background.
+document.body.classList.add("text-text") // set base text colour to --ctp-text
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
