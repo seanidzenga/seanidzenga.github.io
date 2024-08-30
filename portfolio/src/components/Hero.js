c52bbb17c2d6e45bd5aeee862762ed438d6b9298
Sean Idzenga <sean.idzenga@protonmail.com>
2024-08-30 16:51:50 -0400

    Breaking out a 'typewriter' component

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,10 +1,12 @@
+import Typewriter from './Typewriter';
+
 function Hero({ headline }){
   return(
     <section class="py-24 flex items-center min-h-screen justify-center bg-crust">
       <div class="mx-auto max-w-[43rem]">
         <div class="text-center">
           <h1 class="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-lavender">{headline}</h1>
-          <p class="mt-3 text-lg leading-relaxed text-subtext1 code">Software Developer █</p>
+          <Typewriter/>
         </div>
       </div>
     </section>
---
+++ portfolio/src/components/Typewriter.js
@@ -0,0 +1,7 @@
+function Typewriter(){
+  return(
+    <p class="mt-3 text-lg leading-relaxed text-subtext1 code">Software Developer █</p>
+  );
+}
+
+export default Typewriter;
