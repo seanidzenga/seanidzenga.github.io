037f14fd6919877caf8fc1c8e2c97aeb85541983
Sean Idzenga <sean.idzenga@protonmail.com>
2024-08-30 14:06:14 -0400

    Add rough draft of hero component

---
+++ portfolio/src/components/Hero.js
@@ -0,0 +1,14 @@
+function Hero(){
+  return(
+    <section class="py-24 flex items-center min-h-screen justify-center bg-crust">
+      <div class="mx-auto max-w-[43rem]">
+        <div class="text-center">
+          <h1 class="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-lavender">Sean Idzenga</h1>
+          <p class="mt-3 text-lg leading-relaxed text-subtext1 code">Software Developer █</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
