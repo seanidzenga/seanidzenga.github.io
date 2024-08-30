4c5a873e771ec0c04fd81a33eeb3bc7a4972fb45
Sean Idzenga <sean.idzenga@protonmail.com>
2024-08-30 17:11:14 -0400

    Let's make that caret blink!

--- portfolio/src/components/Typewriter.js
+++ portfolio/src/components/Typewriter.js
@@ -1,6 +1,13 @@
+// this component will display some messages
+// these messages will appear to be "typed out" one at a time and holding for some set amount of time
+// before erasing itself and displaying the next message
+
 function Typewriter(){
   return(
-    <p class="mt-3 text-lg leading-relaxed text-subtext1 code">Software Developer █</p>
+    <typewriter class="mt-3 text-lg leading-relaxed text-subtext1 code">
+      Software Developer
+      <span className="blinking-caret"> █</span>
+    </typewriter>
   );
 }
 
