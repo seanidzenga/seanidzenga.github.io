a8af11ba06c2f86942324fa778863bf16364b70a
Sean Idzenga <sean.idzenga@protonmail.com>
2024-09-05 05:55:41 -0400

    quick push of wip

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -2,10 +2,10 @@ import Typewriter from './Typewriter';
 
 function Hero({ headline }){
   return(
-    <section class="py-24 flex items-center min-h-screen justify-center bg-crust">
-      <div class="mx-auto max-w-[43rem]">
-        <div class="text-center">
-          <h1 class="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-lavender">{headline}</h1>
+    <section className="py-24 flex items-center min-h-screen justify-center bg-crust">
+      <div className="mx-auto max-w-[43rem]">
+        <div className="text-center">
+          <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-lavender">{headline}</h1>
           <Typewriter/>
         </div>
       </div>
--- portfolio/src/components/Typewriter.js
+++ portfolio/src/components/Typewriter.js
@@ -1,14 +1,63 @@
-// this component will display some messages
-// these messages will appear to be "typed out" one at a time and holding for some set amount of time
-// before erasing itself and displaying the next message
+import React from 'react';
+import { useState, useEffect } from 'react';
 
-function Typewriter(){
+const Typewriter = () => {
+  
+  const phrases = [
+    "Software Developer",
+    "Disk Jockey",
+    "Game Developer",
+    "Cyber Punk"
+  ];
+  const [buffer, setBuffer] = useState(phrases[0]);
+  const [caretClass, setCaretClass] = useState();
+
+  // let phraseCounter = 0;
+
+  // we'll need an array of strings, these strings will be the "things I am"
+  // we'll store a counter, the counter will reset to 0 once it hits the length of the array
+  // we'll use the counter to find the next string to rewrite
+
+  const UpdateTypewriter = () => {
+
+    if(buffer.length > 0){
+
+      // there's stuff in the buffer, we want to erase it one character at a time
+      setCaretClass('solid-caret');
+
+      let tempBuffer = buffer;
+
+      while(tempBuffer.length > 0){
+        tempBuffer = tempBuffer.slice(0, tempBuffer.length -1);
+        setBuffer(tempBuffer);
+        setTimeout(200);
+      }
+
+      // while(buffer.length > 0){
+        // setBuffer(buffer.slice(0, buffer.length -1));
+        // setTimeout(2000);
+      // }
+      // setCaretClass('blinking-caret');
+    }
+
+    // setCaretClass('blinking-caret');
+  }
+
+  useEffect(() => {
+    const interval = setInterval(
+      UpdateTypewriter,
+      1000,
+    );
+
+    return () => clearInterval(interval);
+  });
+
   return(
-    <typewriter class="mt-3 text-lg leading-relaxed text-subtext1 code">
-      Software Developer
-      <span className="blinking-caret"> █</span>
-    </typewriter>
+    <div className="typewriter mt-3 text-lg leading-relaxed text-subtext1 code">
+      {buffer}
+      <span className={caretClass}>█</span>
+    </div>
   );
-}
+};
 
 export default Typewriter;
