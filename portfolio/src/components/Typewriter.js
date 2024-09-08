822a80956a4e26186d6c3f29df983f6fb21b423a
Sean Idzenga <sean.idzenga@protonmail.com>
2024-09-07 20:45:52 -0400

    Use an enum for directions, don't pause when all text has been erased!

--- portfolio/src/components/Typewriter.js
+++ portfolio/src/components/Typewriter.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const Typewriter = ({writeSpeed = 100, deleteSpeed = 100, pauseDuration = 5000, phrases}) => {
+const Direction = Object.freeze({
+  ERASE:   Symbol("erase"),
+  WRITE:  Symbol("write"),
+  PAUSE: Symbol("pause")
+});
+
+const Typewriter = ({writeSpeed = 100, deleteSpeed = 100, pauseDuration = 3000, phrases}) => {
 
   const [buffer, setBuffer] = useState(phrases[0]);
   const [caretClass, setCaretClass] = useState('blinking-caret');
-  const [direction, setDirection] = useState('erasing');
-  const [prevDirection, setPrevDirection] = useState('erasing');
+  const [direction, setDirection] = useState(Direction.PAUSE);
+  const [prevDirection, setPrevDirection] = useState(Direction.WRITE);
   const [currentPhrase, setCurrentPhrase] = useState(0);
 
   useEffect(() => {
 
     let interval;
 
-    if(direction === 'erasing'){
+    // ERASING STATE
+    if(direction === Direction.ERASE){
 
       setCaretClass('');
-      setPrevDirection('erasing');
+      setPrevDirection(Direction.ERASE);
 
       interval = setInterval(() => {
 
@@ -24,7 +31,7 @@ const Typewriter = ({writeSpeed = 100, deleteSpeed = 100, pauseDuration = 5000, 
   
           if(prevBuffer.length === 0){
   
-            setDirection('pausing');
+            setDirection(Direction.PAUSE);
             clearInterval(interval);
             return '';
           }
@@ -34,10 +41,11 @@ const Typewriter = ({writeSpeed = 100, deleteSpeed = 100, pauseDuration = 5000, 
       }, deleteSpeed);
     }
 
-    if(direction === 'writing'){
+    // WRITING STATE
+    if(direction === Direction.WRITE){
       
       setCaretClass('');
-      setPrevDirection('writing');
+      setPrevDirection(Direction.WRITE);
 
       interval = setInterval(() => {
 
@@ -45,7 +53,7 @@ const Typewriter = ({writeSpeed = 100, deleteSpeed = 100, pauseDuration = 5000, 
 
           if(buffer.length >= phrases[currentPhrase].length){
 
-            setDirection('pausing');
+            setDirection(Direction.PAUSE);
             clearInterval(interval);
             return buffer;
           }
@@ -55,38 +63,42 @@ const Typewriter = ({writeSpeed = 100, deleteSpeed = 100, pauseDuration = 5000, 
       }, writeSpeed);
     }
 
-    if(direction === 'pausing'){
+    // PAUSED STATE
+    if(direction === Direction.PAUSE){
 
-      setCaretClass('blinking-caret');
+      // If the last state before pausing was to write then we'll wait for
+      // `pauseDuration` before switching to erase -
+      // doing so prevents pausing in the erased state.
+      if(prevDirection === Direction.WRITE){
 
-      interval = setInterval(() => {
+        setCaretClass('blinking-caret');
 
-        if(prevDirection === 'writing'){
+        interval = setInterval(() => {
 
-          setDirection('erasing');
-          setPrevDirection('pausing');
+          setPrevDirection(Direction.PAUSE);
+          setDirection(Direction.ERASE);
+          clearInterval(interval);
+  
+        }, pauseDuration);
 
+      } else {
+
+        if(currentPhrase >= phrases.length -1){
+          setCurrentPhrase(0);
         }else{
-
-          if(currentPhrase >= phrases.length -1){
-            setCurrentPhrase(0);
-          }else{
-            setCurrentPhrase(currentPhrase +1);
-          }
-          setDirection('writing');
-          setPrevDirection('pausing');
+          setCurrentPhrase(currentPhrase +1);
         }
+        setDirection(Direction.WRITE);
+        setPrevDirection(Direction.PAUSE);
+      }
+    }
 
+    return () => {
+      if(interval !== null){
         clearInterval(interval);
-
-      }, pauseDuration);
+      }
     }
-
-    if(interval === null){
-      return;
-    }
-
-    return () => clearInterval(interval);
+    
   }, [direction, deleteSpeed, writeSpeed, buffer, phrases, currentPhrase, pauseDuration, prevDirection]);
 
   return(
